f9d6e484bacf985d7567a43b88cddfede095123c
Dan <dalbertico007@gmail.com>
2024-04-23 10:16:53 -0700

    made clause buffer

---
+++ KM10-SAT/KM10-SAT.srcs/sources_1/new/UNSAT_Clause_Table.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/23/2024 09:41:06 AM
+// Design Name: 
+// Module Name: UNSAT_Clause_Table
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UNSAT_Clause_Table(
+
+    );
+endmodule
