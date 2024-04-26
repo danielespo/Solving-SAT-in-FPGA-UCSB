86a5585e5fb7576009c30b0fdf09c1b198d21c0c
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-04-26 01:52:16 -0700

    updated interfacing, unified top

--- KM10-SAT/KM10-SAT.gen/sources_1/bd/mref/Clause_Register/xgui/Clause_Register_v1_0.tcl
+++ KM10-SAT/KM10-SAT.gen/sources_1/bd/mref/Clause_Register/xgui/Clause_Register_v1_0.tcl
@@ -4,7 +4,6 @@ proc init_gui { IPINST } {
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
   ipgui::add_param $IPINST -name "LITERAL_ADDRESS_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "MAX_CLAUSES_PER_VARIABLE" -parent ${Page_0}
   ipgui::add_param $IPINST -name "NSAT" -parent ${Page_0}
 
 
@@ -19,15 +18,6 @@ proc validate_PARAM_VALUE.LITERAL_ADDRESS_WIDTH { PARAM_VALUE.LITERAL_ADDRESS_WI
 	return true
 }
 
-proc update_PARAM_VALUE.MAX_CLAUSES_PER_VARIABLE { PARAM_VALUE.MAX_CLAUSES_PER_VARIABLE } {
-	# Procedure called to update MAX_CLAUSES_PER_VARIABLE when any of the dependent parameters in the arguments change
-}
-
-proc validate_PARAM_VALUE.MAX_CLAUSES_PER_VARIABLE { PARAM_VALUE.MAX_CLAUSES_PER_VARIABLE } {
-	# Procedure called to validate MAX_CLAUSES_PER_VARIABLE
-	return true
-}
-
 proc update_PARAM_VALUE.NSAT { PARAM_VALUE.NSAT } {
 	# Procedure called to update NSAT when any of the dependent parameters in the arguments change
 }
@@ -37,11 +27,6 @@ proc validate_PARAM_VALUE.NSAT { PARAM_VALUE.NSAT } {
 	return true
 }
 
-
-proc update_MODELPARAM_VALUE.MAX_CLAUSES_PER_VARIABLE { MODELPARAM_VALUE.MAX_CLAUSES_PER_VARIABLE PARAM_VALUE.MAX_CLAUSES_PER_VARIABLE } {
-	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.MAX_CLAUSES_PER_VARIABLE}] ${MODELPARAM_VALUE.MAX_CLAUSES_PER_VARIABLE}
-}
 
 proc update_MODELPARAM_VALUE.NSAT { MODELPARAM_VALUE.NSAT PARAM_VALUE.NSAT } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
