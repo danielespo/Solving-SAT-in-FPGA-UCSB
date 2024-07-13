ee8fdb15b2c05a970ec0d79cb3c6bdfba77e3cee
Daniel <dalbertico007@gmail.com>
2024-07-12 19:47:15 -0700

    clean attempt at least we tried

--- rtl/Clause_Selector.v
+++
@@ -1,8 +0,0 @@
-/*
-Clause_Selector.v
-Author: Zeiler Randall-Reed
-
-Description:
-Entirely unimplemented currently. This module was intended to pair with Clause_Processor.v 
-as two sub-top modules. Refer to Clause_Processor.v for more details.
-*/
