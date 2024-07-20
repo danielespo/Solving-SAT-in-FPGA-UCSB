87247cceb40bf012e4888b45e512017562f7c930
unknown <barry_wang02@163.com>
2024-07-19 18:14:17 -0700

    unsat buffer minor fix

--- rtl/bd/Unsat_Clause_Buffer/hdl/Unsat_Clause_Buffer_wrapper.v
+++ rtl/bd/Unsat_Clause_Buffer/hdl/Unsat_Clause_Buffer_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
-//Date        : Fri Jul 19 02:58:41 2024
+//Date        : Fri Jul 19 18:03:48 2024
 //Host        : Barry-Home-PC running 64-bit major release  (build 9200)
 //Command     : generate_target Unsat_Clause_Buffer_wrapper.bd
 //Design      : Unsat_Clause_Buffer_wrapper
@@ -16,7 +16,6 @@ module Unsat_Clause_Buffer_wrapper
     S00_AXI_0_araddr,
     S00_AXI_0_arburst,
     S00_AXI_0_arcache,
-    S00_AXI_0_arid,
     S00_AXI_0_arlen,
     S00_AXI_0_arlock,
     S00_AXI_0_arprot,
@@ -27,7 +26,6 @@ module Unsat_Clause_Buffer_wrapper
     S00_AXI_0_awaddr,
     S00_AXI_0_awburst,
     S00_AXI_0_awcache,
-    S00_AXI_0_awid,
     S00_AXI_0_awlen,
     S00_AXI_0_awlock,
     S00_AXI_0_awprot,
@@ -35,12 +33,10 @@ module Unsat_Clause_Buffer_wrapper
     S00_AXI_0_awready,
     S00_AXI_0_awsize,
     S00_AXI_0_awvalid,
-    S00_AXI_0_bid,
     S00_AXI_0_bready,
     S00_AXI_0_bresp,
     S00_AXI_0_bvalid,
     S00_AXI_0_rdata,
-    S00_AXI_0_rid,
     S00_AXI_0_rlast,
     S00_AXI_0_rready,
     S00_AXI_0_rresp,
@@ -51,15 +47,17 @@ module Unsat_Clause_Buffer_wrapper
     S00_AXI_0_wstrb,
     S00_AXI_0_wvalid,
     clause_addr_i,
-    clause_data_i,
-    clause_data_o,
-    clause_wren);
+    clause_i,
+    clause_o,
+    clause_wren_i,
+    en_i,
+    reset_i,
+    rst_busy_o);
   input ACLK_0;
   input ARESETN_0;
   input [31:0]S00_AXI_0_araddr;
   input [1:0]S00_AXI_0_arburst;
   input [3:0]S00_AXI_0_arcache;
-  input [11:0]S00_AXI_0_arid;
   input [7:0]S00_AXI_0_arlen;
   input [0:0]S00_AXI_0_arlock;
   input [2:0]S00_AXI_0_arprot;
@@ -70,7 +68,6 @@ module Unsat_Clause_Buffer_wrapper
   input [31:0]S00_AXI_0_awaddr;
   input [1:0]S00_AXI_0_awburst;
   input [3:0]S00_AXI_0_awcache;
-  input [11:0]S00_AXI_0_awid;
   input [7:0]S00_AXI_0_awlen;
   input [0:0]S00_AXI_0_awlock;
   input [2:0]S00_AXI_0_awprot;
@@ -78,12 +75,10 @@ module Unsat_Clause_Buffer_wrapper
   output [0:0]S00_AXI_0_awready;
   input [2:0]S00_AXI_0_awsize;
   input [0:0]S00_AXI_0_awvalid;
-  output [11:0]S00_AXI_0_bid;
   input [0:0]S00_AXI_0_bready;
   output [1:0]S00_AXI_0_bresp;
   output [0:0]S00_AXI_0_bvalid;
   output [31:0]S00_AXI_0_rdata;
-  output [11:0]S00_AXI_0_rid;
   output [0:0]S00_AXI_0_rlast;
   input [0:0]S00_AXI_0_rready;
   output [1:0]S00_AXI_0_rresp;
@@ -94,16 +89,18 @@ module Unsat_Clause_Buffer_wrapper
   input [3:0]S00_AXI_0_wstrb;
   input [0:0]S00_AXI_0_wvalid;
   input [12:0]clause_addr_i;
-  input [35:0]clause_data_i;
-  output [35:0]clause_data_o;
-  input clause_wren;
+  input [35:0]clause_i;
+  output [35:0]clause_o;
+  input clause_wren_i;
+  input en_i;
+  input reset_i;
+  output rst_busy_o;
 
   wire ACLK_0;
   wire ARESETN_0;
   wire [31:0]S00_AXI_0_araddr;
   wire [1:0]S00_AXI_0_arburst;
   wire [3:0]S00_AXI_0_arcache;
-  wire [11:0]S00_AXI_0_arid;
   wire [7:0]S00_AXI_0_arlen;
   wire [0:0]S00_AXI_0_arlock;
   wire [2:0]S00_AXI_0_arprot;
@@ -114,7 +111,6 @@ module Unsat_Clause_Buffer_wrapper
   wire [31:0]S00_AXI_0_awaddr;
   wire [1:0]S00_AXI_0_awburst;
   wire [3:0]S00_AXI_0_awcache;
-  wire [11:0]S00_AXI_0_awid;
   wire [7:0]S00_AXI_0_awlen;
   wire [0:0]S00_AXI_0_awlock;
   wire [2:0]S00_AXI_0_awprot;
@@ -122,12 +118,10 @@ module Unsat_Clause_Buffer_wrapper
   wire [0:0]S00_AXI_0_awready;
   wire [2:0]S00_AXI_0_awsize;
   wire [0:0]S00_AXI_0_awvalid;
-  wire [11:0]S00_AXI_0_bid;
   wire [0:0]S00_AXI_0_bready;
   wire [1:0]S00_AXI_0_bresp;
   wire [0:0]S00_AXI_0_bvalid;
   wire [31:0]S00_AXI_0_rdata;
-  wire [11:0]S00_AXI_0_rid;
   wire [0:0]S00_AXI_0_rlast;
   wire [0:0]S00_AXI_0_rready;
   wire [1:0]S00_AXI_0_rresp;
@@ -138,9 +132,12 @@ module Unsat_Clause_Buffer_wrapper
   wire [3:0]S00_AXI_0_wstrb;
   wire [0:0]S00_AXI_0_wvalid;
   wire [12:0]clause_addr_i;
-  wire [35:0]clause_data_i;
-  wire [35:0]clause_data_o;
-  wire clause_wren;
+  wire [35:0]clause_i;
+  wire [35:0]clause_o;
+  wire clause_wren_i;
+  wire en_i;
+  wire reset_i;
+  wire rst_busy_o;
 
   Unsat_Clause_Buffer Unsat_Clause_Buffer_i
        (.ACLK_0(ACLK_0),
@@ -148,7 +145,6 @@ module Unsat_Clause_Buffer_wrapper
         .S00_AXI_0_araddr(S00_AXI_0_araddr),
         .S00_AXI_0_arburst(S00_AXI_0_arburst),
         .S00_AXI_0_arcache(S00_AXI_0_arcache),
-        .S00_AXI_0_arid(S00_AXI_0_arid),
         .S00_AXI_0_arlen(S00_AXI_0_arlen),
         .S00_AXI_0_arlock(S00_AXI_0_arlock),
         .S00_AXI_0_arprot(S00_AXI_0_arprot),
@@ -159,7 +155,6 @@ module Unsat_Clause_Buffer_wrapper
         .S00_AXI_0_awaddr(S00_AXI_0_awaddr),
         .S00_AXI_0_awburst(S00_AXI_0_awburst),
         .S00_AXI_0_awcache(S00_AXI_0_awcache),
-        .S00_AXI_0_awid(S00_AXI_0_awid),
         .S00_AXI_0_awlen(S00_AXI_0_awlen),
         .S00_AXI_0_awlock(S00_AXI_0_awlock),
         .S00_AXI_0_awprot(S00_AXI_0_awprot),
@@ -167,12 +162,10 @@ module Unsat_Clause_Buffer_wrapper
         .S00_AXI_0_awready(S00_AXI_0_awready),
         .S00_AXI_0_awsize(S00_AXI_0_awsize),
         .S00_AXI_0_awvalid(S00_AXI_0_awvalid),
-        .S00_AXI_0_bid(S00_AXI_0_bid),
         .S00_AXI_0_bready(S00_AXI_0_bready),
         .S00_AXI_0_bresp(S00_AXI_0_bresp),
         .S00_AXI_0_bvalid(S00_AXI_0_bvalid),
         .S00_AXI_0_rdata(S00_AXI_0_rdata),
-        .S00_AXI_0_rid(S00_AXI_0_rid),
         .S00_AXI_0_rlast(S00_AXI_0_rlast),
         .S00_AXI_0_rready(S00_AXI_0_rready),
         .S00_AXI_0_rresp(S00_AXI_0_rresp),
@@ -183,7 +176,10 @@ module Unsat_Clause_Buffer_wrapper
         .S00_AXI_0_wstrb(S00_AXI_0_wstrb),
         .S00_AXI_0_wvalid(S00_AXI_0_wvalid),
         .clause_addr_i(clause_addr_i),
-        .clause_data_i(clause_data_i),
-        .clause_data_o(clause_data_o),
-        .clause_wren(clause_wren));
+        .clause_i(clause_i),
+        .clause_o(clause_o),
+        .clause_wren_i(clause_wren_i),
+        .en_i(en_i),
+        .reset_i(reset_i),
+        .rst_busy_o(rst_busy_o));
 endmodule
