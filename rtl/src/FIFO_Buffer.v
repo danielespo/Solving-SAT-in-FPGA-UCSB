beed7e6f383ff4a66e75560a07f9be476b84cbac
unknown <barry_wang02@163.com>
2024-08-06 18:15:00 -0700

    make FIFO tree V2.0

--- rtl/src/FIFO_Buffer.v
+++ rtl/src/FIFO_Buffer.v
@@ -30,8 +30,8 @@ module FIFO_Buffer #(
     input                       clk,            // Clock signal
     input                       reset,          // Reset signal
     input      [DATA_WIDTH-1:0] data_i,         // Data input
-    input                       read_en_i,      // Read signal
-    input                       write_en_i,     // Write signal
+    input                       rden_i,      // Read signal
+    input                       wren_i,     // Write signal
     
     output reg [DATA_WIDTH-1:0] data_o,         // Data output
     output                      empty_o,        // Empty flag
@@ -51,18 +51,18 @@ module FIFO_Buffer #(
             write_ptr       <= 0;
             counter         <= 0;
             data_o          <= 0;
-        end else if (write_en_i && read_en_i) begin         // concurrent rw is always valid
+        end else if (wren_i && rden_i) begin         // concurrent rw is always valid
             buffer[write_ptr]   <= data_i;
             data_o              <= buffer[read_ptr];
             write_ptr           <= write_ptr + 1;
             read_ptr            <= read_ptr + 1;
         end else begin
-            if (write_en_i && !full_o) begin                // if write enable is high and the buffer is not full
+            if (wren_i && !full_o) begin                // if write enable is high and the buffer is not full
                 buffer[write_ptr]   <= data_i;              // write the data to buffer at the write_ptr index
                 write_ptr           <= write_ptr + 1;       // increment write_ptr
                 counter             <= counter + 1;         // increment counter
             end
-            if (read_en_i && !empty_o) begin                // if read enable is high and the buffer is not empty
+            if (rden_i && !empty_o) begin                // if read enable is high and the buffer is not empty
                 data_o          <= buffer[read_ptr];        // output the data at the read_ptr index
                 read_ptr        <= read_ptr + 1;            // increment read_ptr
                 counter         <= counter - 1;             // decrement counter
