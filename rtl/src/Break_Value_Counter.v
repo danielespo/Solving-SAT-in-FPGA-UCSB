0b4a265d0be63e4c8d8252bba46ce0b2e20e4f64
zeilerphone <zeiler@ucsb.edu>
2024-08-18 13:53:54 -0700

    BVC fix - passes testbench

--- rtl/src/Break_Value_Counter.v
+++ rtl/src/Break_Value_Counter.v
@@ -4,6 +4,7 @@ Break_Value_Counter.v
 
 V1.0 Author: Zeiler Randall-Reed
 V2.0 Author: Zeiler Randall-Reed
+V2.1 Author: Zeiler Randall-Reed
 
 Description:
 This module takes the output of the clause evaluators, and clause table mask as an input. It 
@@ -14,13 +15,18 @@ clauses and outputs the break value.
 Notes:
 - 8/16 : according to the diagram, this module is not clocked. It is a combinatorial module that 
     works in conjunction with the Heuristic_Selector and some regs in the Break_Counter_Selector
+- 8/18 : changed the summation method to one that is inefficient but works. see related comment 
+    in the code below. (line 45-47)
 
 Testing:
 V1.0
 - testbench file created 5/23/24
-- all tests passed
+- all tests passed (august)
 V2.0
-- testbench not yet created - might test along with Heuristic_Selector in Break_Counter_Selector.v
+- 8/15: tb not yet created - might test along with Heuristic_Selector in Break_Counter_Selector.v
+V2.1
+- 8/18: tb not yet created - will test standalone and as a part of Break_Counter_Selector.v
+- 8/18: all tests passed
 
 */
 module Break_Value_Counter #(
@@ -37,37 +43,20 @@ module Break_Value_Counter #(
     output wire [NUM_CLAUSES - 1 : 0]       clause_broken_o     // forwarding of bits indicating if the clause is broken / unsatisfied
 );
 
-wire [NUM_CLAUSES_BITS - 1 : 0] break_sum_steps [NUM_CLAUSES - 1 : 0];
+wire [NUM_CLAUSES_BITS - 1 : 0] break_sum_steps [NUM_CLAUSES - 2 : 0];
 
+
+// this has the potential to be slow. if timing issues arise, consider a more efficient summation method
+// - maybe try a tree of some sort. I was having a hard time finding a way to make one that is 
+//   parameterizable, so I chose this simpler (and potentially slower) option.
 genvar i;
 generate
-    for(i = 0; i < NUM_CLAUSES / 2; i = i + 1) begin
-        assign break_sum_steps[i] = clause_broken_i[i * 2] + clause_broken_i[i * 2 + 1];
-    end
-    for(i = 0; i < NUM_CLAUSES / 4; i = i + 1) begin
-        assign break_sum_steps[i + NUM_CLAUSES / 2] = break_sum_steps[i * 2] + break_sum_steps[i * 2 + 1];
-    end
-    assign break_sum_steps[0] = clause_broken_i[0] + clause_broken_i[1];
-    for(i = 0; i < NUM_CLAUSES; i = i + 1) begin
-        assign break_value_o = break_value_o + (clause_broken_i[i] && mask_bits[i]);
+    assign break_sum_steps[0] = (clause_broken_i[0] & mask_bits_i[0]) + (clause_broken_i[1] & mask_bits_i[1]);
+    for(i = 0; i < NUM_CLAUSES - 2; i = i + 1) begin
+        assign break_sum_steps[i + 1] = break_sum_steps[i] + (clause_broken_i[i + 2] & mask_bits_i[i + 2]);
     end
 endgenerate
 
 assign clause_broken_o = clause_broken_i & mask_bits_i;
-
-// always @(posedge clk) begin
-//     if(reset) begin         // on reset, reset the output
-//         break_value_o = 0;
-//         clause_broken_o = 0;
-//     end else begin
-//         break_value_o = 0;  // set the output to 0
-//         for(i = 0; i < NUM_CLAUSES; i = i + 1) begin        // loop through the broken and mask bits
-//             if(clause_broken_i[i] && mask_bits[i]) begin    // if the clause is broken and the mask is valid for a given clause,
-//                 break_value_o = break_value_o + 1'b1;       // increment the break value counter
-//             end
-//         end
-//         clause_broken_o = clause_broken_i;                  // forward the clause broken bits
-//     end
-// end
 
 endmodule
--- rtl/tb/Break_Value_Counter_tb.v
+++ rtl/tb/Break_Value_Counter_tb.v
@@ -1,12 +1,19 @@
-/* 
+/*
+Version: 2.0 
 Break_Value_Counter_tb.v
-Author: Zeiler Randall-Reed
+
+V1.0 Author: Zeiler Randall-Reed
+V2.0 Author: Zeiler Randall-Reed
 
 Description:
 Testbench file for Break_Value_Counter.v
 
 Status:
-Complete
+- V1.0: Complete, passes all tests
+- V2.0: 
+    - In progress (8/18)
+    - all tests passed (8/18)
+
 */
 `timescale 1ns / 1ps
 
@@ -20,17 +27,22 @@ parameter NUM_CLAUSES = 20;
 parameter NUM_ROWS = 3;
 parameter NUM_CLAUSES_BITS = 5;
 
-parameter NUM_TESTS = 10;
+parameter NUM_TESTS_DET = 10; 
+parameter NUM_TESTS_RAN = 40;
+
+parameter NUM_TESTS = NUM_TESTS_DET + NUM_TESTS_RAN;
 
 // inputs
 reg clk = 1;
 always #5 clk <= ~clk;
 reg reset;
 
-reg [NUM_CLAUSES-1:0] clause_status_i;
+reg [NUM_CLAUSES - 1 : 0] clause_status_i;
+reg [NUM_CLAUSES - 1 : 0] mask_bits_i;
 
 // outputs
-wire [NUM_CLAUSES_BITS-1:0] break_value_o;
+wire [NUM_CLAUSES_BITS - 1 : 0] break_value_o;
+wire [NUM_CLAUSES - 1 : 0] clause_broken_o;
 
 // Instantiate the Unit Under Test (UUT)
 Break_Value_Counter #(
@@ -38,22 +50,75 @@ Break_Value_Counter #(
     .NUM_ROWS(NUM_ROWS),
     .NUM_CLAUSES_BITS(NUM_CLAUSES_BITS)
 ) uut (
-    .clk(clk),
-    .reset(reset),
-    .clause(clause_status_i),
-    .break_value(break_value_o)
+    .clause_broken_i(clause_status_i),
+    .mask_bits_i(mask_bits_i),
+    .break_value_o(break_value_o),
+    .clause_broken_o(clause_broken_o)
 );
 
-reg [NUM_CLAUSES-1:0] clause_status[NUM_TESTS-1:0];
-reg [NUM_CLAUSES_BITS-1:0] break_value [NUM_TESTS-1:0];
-integer i, test_index;
+reg [NUM_CLAUSES - 1 : 0]       clause_status   [NUM_TESTS - 1 : 0];
+reg [NUM_CLAUSES - 1 : 0]       mask_bits       [NUM_TESTS - 1 : 0];
+reg [NUM_CLAUSES_BITS - 1 : 0]  break_value     [NUM_TESTS - 1 : 0];
+reg [NUM_CLAUSES - 1 : 0]       clause_broken   [NUM_TESTS - 1 : 0];
+integer i, j;
 
+
+// testing vars
+reg bv_pass;
+reg cb_pass;
+reg [NUM_TESTS - 1 : 0] test_pass;
 
 // 4 tests right now, various random bit streams
 initial begin
-    $readmemb("Break_Value_Counter_tb_status.mem", clause_status);
-    $readmemh("Break_Value_Counter_tb_value.mem", break_value);
-    $display("Break Value Counter Testbench: Begin Simulation");
+$display("Break Value Counter Testbench: Begin Simulation");
+// generate test data
+    $display("Generating test data ...");
+    for(i = 0; i < NUM_TESTS_DET; i = i + 1) begin
+        if(i == 0) begin            // 
+            clause_status[i]    = {20{1'b1}};
+            mask_bits[i]        = {20{1'b0}};
+            break_value[i]      = 0;
+            clause_broken[i]    = {20{1'b0}};
+        end else if(i == 1) begin
+            clause_status[i]    = {20{1'b0}};
+            mask_bits[i]        = {20{1'b0}};
+            break_value[i]      = 0;
+            clause_broken[i]    = {20{1'b0}};
+        end else if(i == 2) begin
+            clause_status[i]    = {10{2'b10}};
+            mask_bits[i]        = {10{2'b01}};
+            break_value[i]      = 0;
+            clause_broken[i]    = {10{1'b0}};
+        end else if(i == 3) begin
+            clause_status[i]    = {{10{1'b1}}, {10{1'b0}}};
+            mask_bits[i]        = {{10{1'b0}}, {10{1'b1}}};
+            break_value[i]      = 0;
+            clause_broken[i]    = {20'b0};
+        end else if(i == 4) begin
+            clause_status[i]    = {20{1'b1}};
+            mask_bits[i]        = {20{1'b1}};
+            break_value[i]      = 20;
+            clause_broken[i]    = {20{1'b1}};
+        end else begin
+            clause_status[i]    = $random;
+            mask_bits[i]        = ~clause_status[i];
+            break_value[i]      = 0;
+            clause_broken[i]    = clause_status[i] & mask_bits[i]; // = 0;
+        end
+    end
+    for(i = NUM_TESTS_DET; i < NUM_TESTS; i = i + 1) begin
+        clause_status[i]    = $random;
+        mask_bits[i]        = $random;
+        break_value[i]      = 0;
+        for(j = 0; j < NUM_CLAUSES; j = j + 1) begin
+            break_value[i]      = break_value[i] + (clause_status[i][j] & mask_bits[i][j]);
+        end
+        clause_broken[i]    = clause_status[i] & mask_bits[i];
+    end
+
+    // initialize values
+    clause_status_i = 0;
+    mask_bits_i = 0;
 
     // reset the system
     reset = 1;
@@ -61,24 +126,47 @@ initial begin
     @(negedge clk);
     reset = 0;
 
-    test_index = 0;
-    for(test_index = 0; test_index < NUM_TESTS; test_index = test_index + 1) begin        
-        // load test values into the clause_status input
-        clause_status_i = clause_status[test_index];
-        // $display("Clause status: %b", clause_status_i);
-        // for(i = 0; i < NUM_CLAUSES; i = i + 1) begin 
-        //     clause_status_i[i] = clause_status[test_index][i];
-        // end 
+    // run the tests
+    i = 0;
+    $display("Running tests...");
+    for(i = 0; i < NUM_TESTS; i = i + 1) begin        
+        // load test values into the inputs
+        clause_status_i = clause_status[i];
+        mask_bits_i = mask_bits[i];
 
-        // wait for the output to be ready
+        bv_pass = 1;
+        cb_pass = 1;
+        test_pass[i] = 1;
+
+        // wait for the output to be ready (does not need to be on clock cycle, but it is a way to do timing)
         @(negedge clk);
 
         // check if the output matches the expected value
-        if(break_value_o != break_value[test_index]) $display("Test failed: Got %d, expected %d", break_value_o, break_value[test_index]);
-        //`ASSERT(break_value_o == break_value[test_index], ("Break Value Counter Testbench: Test failed"));
+        if(break_value_o != break_value[i]) bv_pass = 0;
+        if(clause_broken_o != clause_broken[i]) cb_pass = 0;
+        
+        case({bv_pass, cb_pass})
+            2'b00: $display("Test %d: FAIL: Break Value and Clause Broken do not match", i);
+            2'b01: $display("Test %d: FAIL: Break Value does not match", i);
+            2'b10: $display("Test %d: FAIL: Clause Broken does not match", i);
+            2'b11: $display("Test %d: PASS", i);
+        endcase
+        test_pass[i] = bv_pass & cb_pass;
     end 
     
     $display("Break Value Counter Testbench: End Simulation");
+    $display("Results:");
+    if(~|test_pass) begin
+        $display("    Some tests failed:");
+        for(i = 0; i < NUM_TESTS; i = i + 1) begin
+            if(~test_pass[i]) begin
+                $display("Test %d: FAIL", i);
+            end
+        end
+    end else begin
+        $display("    All tests passed");
+    end
+
 end
 
 
