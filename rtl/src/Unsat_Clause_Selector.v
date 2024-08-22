7a6816ed3b70ee357d4313f9fd06b8ee6d992bf0
zeilerphone <zeiler@ucsb.edu>
2024-08-22 13:05:57 -0700

    initial commit for unsatisfied clause selector

---
+++ rtl/src/Unsat_Clause_Selector.v
@@ -0,0 +1,28 @@
+/*
+Version: 1.0
+Unsat_Clause_Selector.v
+
+Author V1.0: Zeiler Randall-Reed
+
+Description:
+This module handles the logic to select which index of the Unsatisfied Clause Buffer to read from.
+
+Notes:
+
+Testing:
+
+Change Log:
+8/22/2024 - Zeiler Randall-Reed
+    Created file Unsat_Clause_Selector.v
+*/
+
+module Unsat_Clause_Selector # (
+    parameter BUFFER_DEPTH = 2048,
+    parameter RANDOM_NUM_WIDTH = 18,
+) (
+
+);
+
+
+
+endmodule
